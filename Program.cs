add311061a6f3bf2e96068c18dcab2a1cb5a03ee
makandrii <makarov.andrii.10@gmail.com>
2023-05-07 13:26:01 +0300

    delete cors

--- Program.cs
+++ Program.cs
@@ -2,16 +2,6 @@ using BlackJackApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-                      policy =>
-                      {
-                          policy.WithOrigins("https://makandrii.github.io",
-                                              "https://makandrii-blackjack.azurewebsites.net");
-                      });
-});
-
 builder.Services.AddSingleton<GamesService>();
 builder.Services.AddSingleton<DecksService>();
 
@@ -20,8 +10,6 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
-app.UseCors();
 
 app.MapControllers();
 
